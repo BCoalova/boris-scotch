7e0a249dcf16c707e1e5348286d47f7ae383e0ff
Boris Coalova <68432860+BCoalova@users.noreply.github.com>
2020-12-02 00:50:57 -0300

    navbar

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Appnavbar from './componentes/navbar';
+
+let App = () => <Appnavbar />
+
+
+export default App;
---
+++ src/componentes/navbar.js
@@ -0,0 +1,17 @@
+import react from 'react';
+
+let Appnavbar = () => 
+<header>
+    <nav>
+        <div className="brand">
+            <a href="">YaEstá!</a>
+        </div>
+        <ul>
+            <li><a href="">Remeras</a></li>
+            <li><a href="">Buzos</a></li>
+            <li><a href="">Camperas</a></li>
+        </ul>
+    </nav>
+</header>
+
+export default Appnavbar
