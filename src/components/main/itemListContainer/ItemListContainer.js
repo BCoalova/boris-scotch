dd7082d2ef42b83da85d467595fccb30313bdcb5
Boris Coalova <68432860+BCoalova@users.noreply.github.com>
2020-12-10 22:45:24 -0300

    cleanup 2.0

--- src/components/main/itemListContainer/ItemListContainer.js
+++ src/components/main/itemListContainer/ItemListContainer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Item from './item/Item'
 import './itemListContainer.scss';
 
 
@@ -7,22 +6,7 @@ import './itemListContainer.scss';
 const ItemListContainer = () => {
     return(
         <div className="container">
-            {/* <Item 
-                name="Product One"
-                description="Description One"
-                link="#"
-                linkName="Link One"
-                initial="1" 
-                stock="5" 
-            />
-            <Item 
-                name="Product Two"
-                description="Description Two"
-                link="#"
-                linkName="Link Two"
-                initial="1" 
-                stock="10" 
-            /> */}
+            
         </div>
     )
 }
