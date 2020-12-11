87c0122e9eafeb77c4fed0ee9ce96482cd43e666
Boris Coalova <68432860+BCoalova@users.noreply.github.com>
2020-12-10 22:41:21 -0300

    more clrean up

---
+++ src/components/main/itemListContainer/ItemListContainer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Item from './item/Item'
+import './itemListContainer.scss';
+
+
+
+const ItemListContainer = () => {
+    return(
+        <div className="container">
+            {/* <Item 
+                name="Product One"
+                description="Description One"
+                link="#"
+                linkName="Link One"
+                initial="1" 
+                stock="5" 
+            />
+            <Item 
+                name="Product Two"
+                description="Description Two"
+                link="#"
+                linkName="Link Two"
+                initial="1" 
+                stock="10" 
+            /> */}
+        </div>
+    )
+}
+
+export default ItemListContainer;
