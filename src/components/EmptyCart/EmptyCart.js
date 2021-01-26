3b85a600f60c58f64c7950ecb78e75970150c991
Boris Coalova <68432860+BCoalova@users.noreply.github.com>
2021-01-26 09:21:57 -0300

    Mostramos el cartWidget siempre (～￣▽￣)～

--- src/components/EmptyCart/EmptyCart.js
+++ src/components/EmptyCart/EmptyCart.js
@@ -7,7 +7,7 @@ const EmptyCart = () => {
             <h2>Parece que tu carrito esta vacio</h2>
             <p>¿Estás seguro que podés correr el Cyberpunk 2077 en Ultra - 2k - 144fps?</p>
             <p>mmm...</p>
-            <Link className='btn btn-primary' to='/'>Agregá un producto</Link>
+            <Link className='btn btn-primary' to='/'>Mejor agregá un producto</Link>
         </div>
     )
 }
